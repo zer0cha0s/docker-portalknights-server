b1a682f4e70514a1e13cf60445902a70c423a0d7
fpiesche <florian@yellowkeycard.net>
2022-06-27 21:40:24 +0100

    Fix jq query

--- dist/docker-entrypoint.sh
+++ dist/docker-entrypoint.sh
@@ -1,19 +1,18 @@
 echo "Configuring server..."
 
 if [ -z ${ADMIN_PASSWORD} ]; then
-    ADMIN_PASSWORD="$(openssl rand -hex 16)"
+    export ADMIN_PASSWORD="$(openssl rand -hex 16)"
     echo "No admin password specified, setting a random one"
     echo "------------------------------------------------------------------------"
-    echo "---- Admin password: ${ADMIN_PASSWORD}                              ----"
+    echo "---- Admin password: ${ADMIN_PASSWORD}               ----"
     echo "------------------------------------------------------------------------"
 fi
 
-jq '.basicServerData.name='\"${SERVER_NAME}\"'
-    | .gameplayMode='\"${GAMEPLAY_MODE}\"'
-    | .universeSize='\"${UNIVERSE_SIZE}\"'
-    | .admins.credentials.password='\"${ADMIN_PASSWORD}\"'' \
-    /portalknights/server_config_default.json \
-    > /portalknights/server_config.json
+jq '.basicServerData.name=[env.SERVER_NAME]
+    | .gameplayMode=[env.GAMEPLAY_MODE]
+    | .universeSize=[env.UNIVERSE_SIZE]
+    | .admins.credentials.password=[env.ADMIN_PASSWORD]' \
+    /portalknights/server_config_default.json > /portalknights/server_config.json
 
 echo "Starting Portal Knights dedicated server..."
 wine /portalknights/pk_dedicated_server.exe
